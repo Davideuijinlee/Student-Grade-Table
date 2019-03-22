cae2eaeed92c3398fb92cc751af6d3ce9bb4eb1a
Davideuijinlee <Davideuijinlee@gmail.com>
2019-03-21 19:34:22 -0700

    modified sgt

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
 const server = express(); 
-server.use(express.static(__dirname + '/SGT'));
+server.use(express.static(__dirname + '/html'));
 
 
 var insults = [
